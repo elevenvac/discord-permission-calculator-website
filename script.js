d3f81b944fc05aa0eb08c673ec8948ba19f8de1e
Eleven <incealper00@gmail.com>
2021-06-10 15:31:06 +0300

    fix: scripts - new permissions

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
   var perms = {
-            generalCreateInstantInvite: 0x1,
+            generalCreateInvite: 0x1,
             generalKickMembers: 0x2,
             generalBanMembers: 0x4,
             generalAdministrator: 0x8,
             generalManageChannels: 0x10,
-            generalManageServer: 0x20,
+            generalManageGuild: 0x20,
             generalChangeNickname: 0x4000000,
             generalManageNicknames: 0x8000000,
             generalManageRoles: 0x10000000,
             generalManageWebhooks: 0x20000000,
             generalManageEmojis: 0x40000000,
             generalViewAuditLog: 0x80,
+            generalViewGuildInsights: 0x80000,
             textAddReactions: 0x40,
             textReadMessages: 0x400,
             textSendMessages: 0x800,
@@ -21,14 +22,17 @@
             textReadMessageHistory: 0x10000,
             textMentionEveryone: 0x20000,
             textUseExternalEmojis: 0x40000,
+            textUseSlashCommands: 0x80000000,
             voiceViewChannel: 0x400,
             voiceConnect: 0x100000,
             voiceSpeak: 0x200000,
+            voiceStream: 0x200,
             voiceMuteMembers: 0x400000,
             voiceDeafenMembers: 0x800000,
             voiceMoveMembers: 0x1000000,
             voiceUseVAD: 0x2000000,
-            voicePrioritySpeaker: 0x100
+            voicePrioritySpeaker: 0x100,
+            voiceStageRequestSpeak: 0x100000000
         };
 
         function recalculate(element, perm, noSet) {
